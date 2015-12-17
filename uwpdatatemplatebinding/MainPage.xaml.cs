77d2e6b79c0e1233cb9b387b482402e0256ca20b
Mikael Koskinen <mijapkos@gmail.com>
2015-12-17 15:11:13 +0200

    Example app

---
+++ uwpdatatemplatebinding/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace uwpdatatemplatebinding
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private ObservableCollection<MyUserControlVm> coll;
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            coll = new ObservableCollection<MyUserControlVm>();
+            coll.Add(new MyUserControlVm("http://libcloud.readthedocs.org/en/latest/_images/azure.jpg"));
+            coll.Add(new MyUserControlVm("http://www.nimbo.com/wp-content/uploads/windows-azure-logo-nimbo1.png"));
+
+            this.Flip.ItemsSource = coll;
+
+            base.OnNavigatedTo(e);
+        }
+    }
+
+
+}
---
+++ uwpdatatemplatebinding/MyUserControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace uwpdatatemplatebinding
+{
+    public sealed partial class MyUserControl : UserControl
+    {
+        public static readonly DependencyProperty FigurinePathProperty = DependencyProperty.Register(
+            "FigurinePath", typeof (Uri), typeof (MyUserControl), new PropertyMetadata(default(Uri)));
+
+        public Uri FigurinePath
+        {
+            get { return (Uri) GetValue(FigurinePathProperty); }
+            set { SetValue(FigurinePathProperty, value); }
+        }
+
+        public MyUserControl()
+        {
+            this.InitializeComponent();
+            (this.Content as FrameworkElement).DataContext = this;
+        }
+    }
+    public class MyUserControlVm
+    {
+        public Uri Path { get; set; }
+
+        public MyUserControlVm(string url)
+        {
+            Path = new Uri(url);
+        }
+
+        public void VmAction()
+        {
+            
+        }
+    }
+
+}
